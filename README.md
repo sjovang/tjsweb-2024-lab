fba66759d11752df90af8a835e23c697efbb7bd0
Trond Jakob Sjøvang <trond@sjovang.no>
2024-06-09 12:27:22 +0000

    colors and stuff

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tjsweb-2024-lab
 lab  workspace for a new website
+
+
+        'summer': {
+          100: '#FFFFDD',
+          200: '#D2DE32',
+          300: '#A2C579',
+          400: '#016A70'
+        }
---
+++ assets/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,css}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      'serif': ['ui-serif', 'serif'],
+      'mono': ['IBM Plex Mono', 'ui-monospace', 'monospace']
+    },
+    extend: {
+      colors: {
+        'sage': {
+          100: '#D2E3C8',
+          200: '#86A789',
+          300: '#739072',
+          400: '#4F6F52'
+        },
+        'summer': {
+          100: '#E6FF94',
+          200: '#9DDE8B',
+          300: '#40A578',
+          400: '#006769'
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
